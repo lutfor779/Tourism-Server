85a18f95db62eb9762a13314ec52e72693d8ac89
lutfor779 <mdlrrahman779@gmail.com>
2021-10-31 01:42:01 +0600

    delete order done

--- index.js
+++ index.js
@@ -120,6 +120,15 @@ async function run() {
             res.json(result);
         })
 
+        app.delete('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+
+            console.log('deleting booking with id', result);
+            res.json(result);
+        })
+
 
     }
     finally {
